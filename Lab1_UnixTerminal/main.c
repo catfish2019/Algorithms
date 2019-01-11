88f5e67d1eb97a166f74866dbac2e327306d702e
Rouni Yin <yinrouni@login-students.ccs.neu.edu>
2019-01-10 21:14:17 -0500

    solution

--- Lab1_UnixTerminal/main.c
+++ Lab1_UnixTerminal/main.c
@@ -1,1 +1,17 @@
 // Add your program here!
+# include <stdio.h>
+double power(double base, double n){
+	double result = 1;
+	for (int i = 1; i <= n; i ++){
+		result = result * base;
+	}
+	return result;
+}
+
+int main(){
+for (int i = 1; i < 11; i++){
+double p = power(2, i);
+printf("%f\n", p);
+}
+return 0;
+}
