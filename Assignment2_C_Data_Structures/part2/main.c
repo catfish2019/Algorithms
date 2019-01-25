159a87b9d3427cc32cadd6675c96f94c1a11c4fa
Rouni Yin <yinrouni@login-students.ccs.neu.edu>
2019-01-24 22:55:14 -0500

    upload test

--- Assignment2_C_Data_Structures/part2/main.c
+++ Assignment2_C_Data_Structures/part2/main.c
@@ -19,23 +19,80 @@
 // You can add as many unit tests as you like
 // We will be adding our own to test your program.
 void unitTest1(){
-
-	stack_t* test1 = create_stack(MAX_DEPTH);
-	printf("Attempting to push %d\n",1);
-	stack_enqueue(test1,1);	
-	printf("Removing: %d\n",stack_dequeue(test1));	
-
+	
+	
+	stack_t* test1 = create_stack(3);
+	if (stack_empty(test1)){
+		printf("This is an empty stack.\n");
+   	 }
+	printf("Stack size: %d\n", stack_size(test1));
+   	printf("Attempting to push 1, pushing status: %d\n",stack_enqueue(test1, 1));
+   	printf("Stack size: %d\n", stack_size(test1));
+	printf("Attempting to push 2, pushing status: %d\n",stack_enqueue(test1, 2));
+   	printf("Stack size: %d\n", stack_size(test1));
+	printf("Attempting to push 3, pushing status: %d\n",stack_enqueue(test1, 3));
+	printf("Stack size: %d\n", stack_size(test1));
+	printf("Attempting to push 4, pushing status: %d\n",stack_enqueue(test1, 4));
+	printf("Stack size: %d\n", stack_size(test1));
+	if (stack_full(test1)){
+        	printf("This is a full stack.\n");
+   	 }
+	printf("The size is %d\n", stack_size(test1));
+	printf("Removing: %d\n",stack_dequeue(test1));
+   	printf("The size is %d\n", stack_size(test1));
+    	printf("Removing: %d\n",stack_dequeue(test1));
+    	printf("The size is %d\n", stack_size(test1));
+    	printf("Removing: %d\n",stack_dequeue(test1));
+	if (stack_empty(test1)){
+     	   printf("This is an empty stack.\n");
+    }
 	free_stack(test1);
 }
 
+void unitTest2(){
+	stack_t* test2 = create_stack(35);
+	printf("Stack capactity: %d\n", test2->capacity);
+	free_stack(test2);
+}
+void unitTest3(){
+	stack_t* test3 = create_stack(2);
+	if (stack_empty(test3)){
+        printf("This is an empty stack.\n");
+    }
+	printf("Attempting to push 1, pushing status: %d\n",stack_enqueue(test3, 1));
+	printf("Attempting to push 2, pushing status: %d\n",stack_enqueue(test3, 2));
+	if (stack_full(test3)){
+        printf("This is a full stack.\n");
+    }
+   	printf("The size is %d\n", stack_size(test3));
+    	printf("Removing: %d\n",stack_dequeue(test3));
+    	printf("The size is %d\n", stack_size(test3));
+    	printf("Removing: %d\n",stack_dequeue(test3));
+   	printf("The size is %d\n", stack_size(test3));
 
+	//Dequeue from an empty stack. Exit the program here. 
+    	printf("Removing: %d\n",stack_dequeue(test3));
+	printf("The program is still on.");	
+	free_stack(test3);
+}
+void unitTest4(){
+	stack_t* test4;
+	if (test4 == NULL){
+	printf("NULL\n");
+	}printf("Size: %d\n",stack_size(test4));
+}
 // ====================================================
 // ================== Program Entry ===================
 // ====================================================
 int main(){
 
-	// List of Unit Tests to test your data structure	
+	// List of Unit Tests to test your data structure
 	unitTest1();
-
+	printf("=======================================");
+	unitTest2();
+	printf("=======================================");
+	unitTest3();
+	printf("=======================================");
+	unitTest4();
 	return 0;
 }
