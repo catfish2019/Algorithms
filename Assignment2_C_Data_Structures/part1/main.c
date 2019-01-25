a194cab9d12f3a5fe5760256dacbed50e9318af1
Rouni Yin <yinrouni@login-students.ccs.neu.edu>
2019-01-24 22:57:12 -0500

    upload test

--- Assignment2_C_Data_Structures/part1/main.c
+++ Assignment2_C_Data_Structures/part1/main.c
@@ -18,24 +18,111 @@
 // A sample test of your program
 // You can add as many unit tests as you like
 // We will be adding our own to test your program.
-void unitTest1(){
+void unitTest1(){queue_t* test1 = create_queue(4);
 
-	queue_t* test1 = create_queue(1);
-	printf("Attempting to add %d\n",1);
-	queue_enqueue(test1,1);	
-	printf("Removing: %d\n",queue_dequeue(test1));	
+    if (queue_full(test1)){
+        printf("This is a full queue.\n");}
+    printf("Adding %d, status: %d\n", 1, queue_enqueue(test1, 1));
+    printf("The size is %d.\n", queue_size(test1));
+    
+    if (queue_full(test1)){
+        printf("This is a full queue.\n");}
+    printf("Adding %d, status: %d\n", 2, queue_enqueue(test1, 2));
+    printf("The size is %d.\n", queue_size(test1));
+    
+    if (queue_full(test1)){
+        printf("This is a full queue.\n");}
+    printf("Adding %d, status: %d\n", 3, queue_enqueue(test1, 3));
+    printf("The size is %d.\n", queue_size(test1));
+    
+    if (queue_full(test1)){
+        printf("This is a full queue.\n");}
+    printf("Adding %d, status: %d\n" , 4, queue_enqueue(test1, 4));
+    printf("The size is %d.\n", queue_size(test1));
+    
 
-	free_queue(test1);
+    
+    if (queue_empty(test1)){
+        printf("This is an empty queue.\n");}
+    
+    printf("Removing: %d\n",queue_dequeue(test1));
+    printf("The size is %d.\n", queue_size(test1));
+    printf("Removing: %d\n",queue_dequeue(test1));
+    printf("The size is %d.\n", queue_size(test1));
+    printf("Removing: %d\n",queue_dequeue(test1));
+    printf("The size is %d.\n", queue_size(test1));
+    printf("Removing: %d\n",queue_dequeue(test1));
+    printf("The size is %d.\n", queue_size(test1));
+    
+    free_queue(test1);
+    
+    
+    
 }
+void unitTest2(){
 
+    queue_t* test2 = create_queue(1);
+
+    if (queue_full(test2)){
+        printf("This is a full queue.\n");}
+    printf("Adding %d, status: %d\n", 1, queue_enqueue(test2, 1));
+    printf("The size is %d.\n", queue_size(test2));
+    
+    if (queue_full(test2)){
+        printf("This is a full queue.\n");}
+    printf("Adding %d, status: %d\n", 2, queue_enqueue(test2, 2));
+    printf("The size is %d.\n", queue_size(test2));
+    
+    
+    if (queue_empty(test2)){
+        printf("This is an empty queue.\n");}
+    
+    printf("Removing: %d\n",queue_dequeue(test2));
+    if (queue_empty(test2)){
+        printf("This is an empty queue.\n");}
+    printf("The size is %d.\n", queue_size(test2));
+    printf("Removing: %d\n",queue_dequeue(test2));
+    printf("The size is %d.\n", queue_size(test2));
+    
+    free_queue(test2);
+    
+
+    
+    
+}
+void unitTest3(){
+ // test queue_empty and dequeue an empty queue
+    queue_t* test3 = create_queue(0);
+
+    if (queue_full(test3)){
+        printf("This is a full queue.\n");}
+    printf("Adding %d, status: %d\n", 1, queue_enqueue(test3, 1));
+    printf("Removing: %d\n", queue_dequeue(test3));
+    printf("The size is %d.\n", queue_size(test3));
+    
+    
+    if (queue_empty(test3)){
+        printf("This is an empty queue.\n");}
+
+    free_queue(test3);
+
+    
+    
+}
 
 // ====================================================
 // ================== Program Entry ===================
 // ====================================================
 int main(){
+// List of Unit Tests to test your data structure
+unitTest1();
+    printf("====================================================\n");
+    unitTest3();
+    printf("====================================================\n");
+    unitTest2();
+    
 
-	// List of Unit Tests to test your data structure	
-	unitTest1();
-
-	return 0;
+    
+    return 0;
 }
+
