3bc5cec8a0a9f9db17d3da958a340ab005aa1328
Rouni Yin <yinrouni@login-students.ccs.neu.edu>
2019-02-01 15:21:13 -0500

    upload solution

--- Lab4_RecursiveStrategy/factorial.c
+++ Lab4_RecursiveStrategy/factorial.c
@@ -1,24 +1,37 @@
-// gcc factorial.c -o factorial
+// gcc factorial.c -o factoriali
 // ./factorial
 
 #include <stdio.h>
 
 // TODO: Implement iterative solution here
-int factorial(int n){
+long factorial(int n){
+	long factorial = 1;
+	if (n > 0) {
+		for (int i = 1; i <= n; i++){
+			factorial = factorial * i;
+		}
+		return factorial;
+	}
+		else{
+			return 0;
+		}
+}
 
-  return 0;
-};
 
 // TODO: Implement recursive solution here
-int factorial_rec(int n){
-
-  return 0;
+long factorial_rec(int n){
+	if (n > 0){
+		return n * factorial_rec(n - 1);
+	}
+	else{
+		return 1;
+	}
 }
 
 int main(){
 
-  printf("factorial(10) = %d\n",factorial(10));
-  printf("factorial_rec(10) = %d\n",factorial_rec(10));
+  printf("factorial(10) = %ld\n",factorial(10));
+  printf("factorial_rec(10) = %ld\n",factorial_rec(10));
 
 
   return 0;
