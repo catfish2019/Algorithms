de9836f5e2409424ea737ea056e8fd1f9ea29d6b
Rouni Yin <yinrouni@login-students.ccs.neu.edu>
2019-02-06 17:42:51 -0500

    upload solution

--- Assignment4_BinarySearchTree/part1/main.c
+++ Assignment4_BinarySearchTree/part1/main.c
@@ -18,7 +18,26 @@
 // ================== Program Entry ===================
 // ====================================================
 int main(){
-    bst_t * testBST = create_bst();
-    free_bst(testBST);    
-    return 0;
+	bst_t * testBST = create_bst();
+	bst_add(testBST, 20);
+    	bst_add(testBST, 10);
+    	bst_add(testBST, 30);
+    	bst_add(testBST, 15);
+    	bst_add(testBST, 5);
+    	bst_add(testBST, 25);
+    	bst_add(testBST, 27);
+	printf("=======dfs=======\n");
+	dfs(testBST->root);
+    	printf("====ascending====\n");
+	bst_print(testBST, 0);
+	printf("Sum: %d\n", sum(testBST));
+    
+    	printf("Find 16, status: %d\n", find(testBST, 16));
+	printf("Find 15, status: %d\n", find(testBST, 15));
+	printf("====descending====\n");
+	bst_print(testBST, 1);
+	printf("Size: %d\n", bst_size(testBST));
+
+	free_bst(testBST);    
+	return 0;
 }
